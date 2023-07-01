4ed0c0f2c382bcde1f51603368cc0421aecc4d6c
MartianAcademy <martianacademy@gmail.com>
2023-07-01 17:41:11 +0530

    no network error page updated with satelite image

--- src/components/ProtectedRoute/UnSupportedNetworkComponent.tsx
+++ src/components/ProtectedRoute/UnSupportedNetworkComponent.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { Heading, VStack } from '@chakra-ui/react'
-import { ConnectWalletButton } from '../ConnectWalletButton'
-import SwitchNetworkButton from '../SwitchNetworkButton'
+import React from 'react';
+import { Heading, Image, VStack } from '@chakra-ui/react';
+import { ConnectWalletButton } from '../ConnectWalletButton';
+import SwitchNetworkButton from '../SwitchNetworkButton';
+import { motion } from 'framer-motion';
+
+const MotionImage = motion(Image);
 
 function UnSupportedNetworkComponent() {
   return (
     <VStack spacing={5}>
       <VStack>
-        <Heading color="red" textAlign="center">UnSupported Network!</Heading>
-        <Heading size="md" textAlign="center">Please switch to supported network.</Heading>
+        <MotionImage
+          animate={{
+            y: [1, 7, 1],
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 2,
+          }}
+          src="/errors-svgs/noNetworkSatelite.svg"
+          w={[200, 300]}
+        ></MotionImage>
+        <Heading color="red" textAlign="center">
+          UnSupported Network!
+        </Heading>
+        <Heading size="md" textAlign="center">
+          Please switch to supported network.
+        </Heading>
       </VStack>
       <SwitchNetworkButton />
     </VStack>
-  )
+  );
 }
 
-export default UnSupportedNetworkComponent
+export default UnSupportedNetworkComponent;
